732982d4e465f2c93d35178861390e925feb1aac
Simon Greter <greter@tasix.ch>
2019-08-13 08:42:41 +0200

    Support for PDF options added

--- Html2PdfChromium/Controllers/Html2PdfController.cs
+++ Html2PdfChromium/Controllers/Html2PdfController.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PuppeteerSharp;
+using PuppeteerSharp.Media;
 
 namespace Html2PdfChromium.Controllers
 {
@@ -26,22 +27,66 @@ namespace Html2PdfChromium.Controllers
             {
                 Headless = true,
                 Args = new string[] {"--no-sandbox", "--disable-setuid-sandbox"},
-//                ExecutablePath = "/usr/bin/chromium-browser"
-//                ExecutablePath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
             });
             var page = await browser.NewPageAsync();
             await page.GoToAsync(parameters.bodyUrl);
-            var file = Path.GetTempFileName();
-            await page.PdfAsync(file);
+            var pdfOptions = new PdfOptions();
+            pdfOptions.Scale = parameters.Scale;
+            pdfOptions.DisplayHeaderFooter = parameters.DisplayHeaderFooter;
+            pdfOptions.HeaderTemplate = parameters.HeaderTemplate;
+            pdfOptions.FooterTemplate = parameters.FooterTemplate;
+            pdfOptions.PrintBackground = parameters.PrintBackground;
+            pdfOptions.Landscape = parameters.Landscape;
+            pdfOptions.PageRanges = parameters.PageRanges;
+            pdfOptions.Format = ConvertToPaperFormat(parameters.PaperFormat);
+            pdfOptions.MarginOptions = new MarginOptions
+            {
+                Top = parameters.TopMargin,
+                Left = parameters.LeftMargin,
+                Bottom = parameters.BottomMargin,
+                Right = parameters.RightMargin
+            };
+            pdfOptions.PreferCSSPageSize = parameters.PreferCSSPageSize;
+            
+            var pdf = await page.PdfStreamAsync(pdfOptions);
             var closePageTask = page.CloseAsync();
             
-            var content = await System.IO.File.ReadAllBytesAsync(file);
-            var stream = new MemoryStream(content);
-            System.IO.File.Delete(file);
-
             await closePageTask;
             
-            return new FileStreamResult(stream, "application/pdf") {FileDownloadName = "document.pdf"};
+            return new FileStreamResult(pdf, "application/pdf") {FileDownloadName = "document.pdf"};
+        }
+
+        private PaperFormat ConvertToPaperFormat(string paperFormat)
+        {
+            switch (paperFormat)
+            {
+                case null:
+                    return null;
+                case "Letter":
+                    return PaperFormat.Letter;
+                case "Legal":
+                    return PaperFormat.Legal;
+                case "Tabloid":
+                    return PaperFormat.Tabloid;
+                case "Ledger":
+                    return PaperFormat.Ledger;
+                case "A0":
+                    return PaperFormat.A0;
+                case "A1":
+                    return PaperFormat.A1;
+                case "A2":
+                    return PaperFormat.A2;
+                case "A3":
+                    return PaperFormat.A3;
+                case "A4":
+                    return PaperFormat.A4;
+                case "A5":
+                    return PaperFormat.A5;
+                case "A6":
+                    return PaperFormat.A6;
+                default:
+                    return null;
+            }
         }
     }
 }
--- Html2PdfChromium/PdfParameters.cs
+++ Html2PdfChromium/PdfParameters.cs
@@ -1,7 +1,81 @@
+using System;
+using PuppeteerSharp.Media;
+
 namespace Html2PdfChromium
 {
     public class PdfParameters
     {
-        public string bodyUrl;
+        /// <summary>
+        /// URL to the page that get's converted
+        /// </summary>
+        public string bodyUrl { get; set; }
+
+        /// <summary>
+        /// Scale of the webpage rendering. Defaults to <c>1</c>. Scale amount must be between 0.1 and 2.
+        /// </summary>
+        public Decimal Scale { get; set; } = Decimal.One;
+
+        /// <summary>
+        /// Display header and footer. Defaults to <c>false</c>
+        /// </summary>
+        public bool DisplayHeaderFooter { get; set; } = false;
+
+        /// <summary>
+        /// HTML template for the print header. Should be valid HTML markup with following classes used to inject printing values into them:
+        ///   <c>date</c> - formatted print date
+        ///   <c>title</c> - document title
+        ///   <c>url</c> - document location
+        ///   <c>pageNumber</c> - current page number
+        ///   <c>totalPages</c> - total pages in the document
+        /// </summary>
+        public string HeaderTemplate { get; set; } = string.Empty;
+
+        /// <summary>
+        /// HTML template for the print footer. Should be valid HTML markup with following classes used to inject printing values into them:
+        ///   <c>date</c> - formatted print date
+        ///   <c>title</c> - document title
+        ///   <c>url</c> - document location
+        ///   <c>pageNumber</c> - current page number
+        ///   <c>totalPages</c> - total pages in the document
+        /// </summary>
+        public string FooterTemplate { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Print background graphics. Defaults to <c>false</c>
+        /// </summary>
+        public bool PrintBackground { get; set; } = false;
+
+        /// <summary>
+        /// Paper orientation.. Defaults to <c>false</c>
+        /// </summary>
+        public bool Landscape { get; set; } = false;
+
+        /// <summary>
+        /// Paper ranges to print, e.g., <c>1-5, 8, 11-13</c>. Defaults to the empty string, which means print all pages
+        /// </summary>
+        public string PageRanges { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Paper format. Valid formats: Letter, Legal, Tabloid, Ledger, A0, A1, A2, A3, A4, A5, A6 />
+        /// </summary>
+        public string PaperFormat { get; set; } = "A4";
+
+        /// <summary>Top margin, accepts values labeled with units, defaults to none</summary>
+        public string TopMargin { get; set; }
+
+        /// <summary>Left margin, accepts values labeled with units, defaults to none</summary>
+        public string LeftMargin { get; set; }
+
+        /// <summary>Bottom margin, accepts values labeled with units, defaults to none</summary>
+        public string BottomMargin { get; set; }
+
+        /// <summary>Right margin, accepts values labeled with units, defaults to none</summary>
+        public string RightMargin { get; set; }
+
+        /// <summary>
+        /// Give any CSS <c>@page</c> size declared in the page priority over what is declared in <c>width</c> and <c>height</c> or <c>format</c> options.
+        /// Defaults to <c>false</c>, which will scale the content to fit the paper size.
+        /// </summary>
+        public bool PreferCSSPageSize { get; set; } = false;
     }
 }
