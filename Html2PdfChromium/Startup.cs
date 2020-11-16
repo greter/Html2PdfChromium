a5c5ff5d83e68382ed6f6297c1a94151b4a5f8fb
Simon Greter <greter@tasix.ch>
2020-11-16 15:55:45 +0100

    Switch to .NET 5

--- Html2PdfChromium/Startup.cs
+++ Html2PdfChromium/Startup.cs
@@ -1,13 +1,20 @@
-﻿using Hangfire;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
 using Hangfire.MemoryStorage;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace Html2PdfChromium
+namespace WebApplication
 {
     public class Startup
     {
@@ -21,51 +28,103 @@ namespace Html2PdfChromium
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddJsonOptions(options =>
-            {
-                options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
-            });
-            
+            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Html2Pdf API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "html2pdf", Version = "v1"});
             });
-            
+
             services.AddHangfire(configuration => configuration
                 .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
                 .UseSimpleAssemblyNameTypeSerializer()
                 .UseRecommendedSerializerSettings()
                 .UseMemoryStorage());
-            
-            services.AddHangfireServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication v1"));
             }
 
             app.UseHttpsRedirection();
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Html2Pdf API V1");
-            });
-            
-            app.UseHangfireDashboard();
-            
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
+
+// using Hangfire;
+// using Hangfire.MemoryStorage;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.OpenApi.Models;
+//
+// namespace Html2PdfChromium
+// {
+//     public class Startup
+//     {
+//         public Startup(IConfiguration configuration)
+//         {
+//             Configuration = configuration;
+//         }
+//
+//         public IConfiguration Configuration { get; }
+//
+//         // This method gets called by the runtime. Use this method to add services to the container.
+//         public void ConfigureServices(IServiceCollection services)
+//         {
+//             services.AddMvc()
+//                 .SetCompatibilityVersion(CompatibilityVersion.Latest);
+//             
+//             services.AddSwaggerGen(c =>
+//             {
+//                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Html2Pdf API", Version = "v1" });
+//             });
+//             
+//             services.AddHangfire(configuration => configuration
+//                 .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+//                 .UseSimpleAssemblyNameTypeSerializer()
+//                 .UseRecommendedSerializerSettings()
+//                 .UseMemoryStorage());
+//             
+//             services.AddHangfireServer();
+//         }
+//
+//         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//         {
+//             if (env.IsDevelopment())
+//             {
+//                 app.UseDeveloperExceptionPage();
+//             }
+//             else
+//             {
+//                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//                 app.UseHsts();
+//             }
+//
+//             app.UseHttpsRedirection();
+//
+//             app.UseSwagger();
+//             app.UseSwaggerUI(c =>
+//             {
+//                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Html2Pdf API V1");
+//             });
+//             
+//             app.UseHangfireDashboard();
+//         }
+//     }
+// }
